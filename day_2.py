31a0d3d5558dd969496389ffc9dbcaf8261b54f3
RickForce <Rick@Rickforce.com>
2021-12-02 08:18:03 +0100

    more honest measurement

--- day_2.py
+++ day_2.py
@@ -56,13 +56,10 @@ def split_command_and_value(list_str: list[str]) -> list[list[str, int]]:
     return [[a, int(b)] for a, b in [line.split(' ') for line in list_str]]
 
 
-# moved this out so I can properly measure runtime
-def prepare_input(filename):
-    return split_command_and_value(filename)
-
-
 @util.get_runtime
-def get_solutions(command_value_list):
+def get_solutions(filename):
+    command_value_list = split_command_and_value(filename)
+
     sub_a = Submarine(directions=command_value_list, manual_version='a')
     sub_b = Submarine(directions=command_value_list, manual_version='b')
 
@@ -74,5 +71,5 @@ def get_solutions(command_value_list):
 
 
 if __name__ == '__main__':
-    get_solutions(prepare_input(test_directions))
-    get_solutions(prepare_input(day_2_directions))
+    get_solutions(test_directions)
+    get_solutions(day_2_directions)
