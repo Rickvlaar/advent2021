8227a4f6f5ce5271e12b1c16177115a9ec4dba14
RickForce <Rick@Rickforce.com>
2021-12-04 16:28:47 +0100

    refactor day 3

--- day_3.py
+++ day_3.py
@@ -19,7 +19,7 @@ test_input = ['00100',
 
 
 def reshape_array(report: List[any]) -> np.ndarray:
-    reshaped = [list(line) for line in report]
+    reshaped = [[int(char) for char in line] for line in report]
     return np.transpose(reshaped)
 
 
@@ -30,58 +30,69 @@ def report_to_bin_array(report: List[any]) -> list[int]:
 
 @util.get_runtime
 def get_power_rate(transposed_array: list[int], half_bit_length: int) -> int:
-    gamma_rate = ''
-    epsilon_rate = ''
+    gamma_rate = []
+    epsilon_rate = []
     for line in transposed_array:
-        gamma_rate += get_most_common_bit(line, half_bit_length)
-        epsilon_rate += get_least_common_bit(line, half_bit_length)
-    return int(gamma_rate, 2) * int(epsilon_rate, 2)
+        gamma_rate.append(get_most_common_bit(line, half_bit_length))
+        epsilon_rate.append(get_least_common_bit(line, half_bit_length))
+
+    gamma_rate = convert_bitlist_to_bit(gamma_rate)
+    epsilon_rate = convert_bitlist_to_bit(epsilon_rate)
+
+    return gamma_rate * epsilon_rate
+
+
+def convert_bitlist_to_bit(bitlist):
+    out = 0
+    for bit in bitlist:
+        out = (out << 1) | int(bit)
+    return int(out)
 
 
 @util.get_runtime
-def get_life_support_rate(report: List[str]) -> int:
-    oxygen_rating = filter_array(report, get_most_common_bit)
-    co2_rating = filter_array(report, get_least_common_bit)
+def get_life_support_rate(transposed_array) -> int:
+    oxygen_rating = filter_array(transposed_array, get_most_common_bit)
+    co2_rating = filter_array(transposed_array, get_least_common_bit)
     return oxygen_rating * co2_rating
 
 
-def filter_array(report: List[str], filter_func: Callable) -> int:
-    input_report = [list(line) for line in report]
-    transposed_array = reshape_array(report)
-
-    for bit_index in range(len(transposed_array)):
-        line = transposed_array[bit_index]
-        bits = int(''.join(line), 2)
-        limit = int(round((len(line) / 2 + 0.1)))
+def filter_array(transposed_array, filter_func: Callable) -> int:
+    ind = 0
+    while 1:
+        line = transposed_array[ind]
+        bits = convert_bitlist_to_bit(line)
+        limit = int(round((len(line) / 2) + .1))
         wanted_val = filter_func(bits, limit)
-        lines_kept = [input_report[index] for index, val in enumerate(line) if val == wanted_val]
-        transposed_array = reshape_array(lines_kept)
-        input_report = lines_kept
-        if len(lines_kept) == 1:
+        delete_indeces = [index for index, val in enumerate(line) if val != wanted_val]
+        transposed_array = np.delete(transposed_array, delete_indeces, 1)
+        ind += 1
+        if len(transposed_array[0]) == 1:
             break
 
-    return int(''.join(input_report[0]), 2)
+    return convert_bitlist_to_bit(transposed_array.transpose().flatten())
 
 
-def get_most_common_bit(bits: int, limit: int) -> str:
-    return '1' if bits.bit_count() >= limit else '0'
+def get_most_common_bit(bits: int, limit: int) -> int:
+    return 1 if bits.bit_count() >= limit else 0
 
 
-def get_least_common_bit(bits: int, limit: int) -> str:
-    return '0' if bits.bit_count() >= limit else '1'
+def get_least_common_bit(bits: int, limit: int) -> int:
+    return 0 if bits.bit_count() >= limit else 1
 
 
 if __name__ == '__main__':
-    test_half_bit_length = int(len(test_input) / 2)
-    test_array = report_to_bin_array(test_input)
-    test_power_rate = get_power_rate(test_array, test_half_bit_length)
-    test_life_support_rate = get_life_support_rate(test_input)
-    util.console.print(f'test solution 3A: {test_power_rate}')
-    util.console.print(f'test solution 3B: {test_life_support_rate}\n')
+    # test_half_bit_length = int(len(test_input) / 2)
+    # test_array = report_to_bin_array(test_input)
+    # test_power_rate = get_power_rate(test_array, test_half_bit_length)
+    # test_life_support_rate = get_life_support_rate(test_input)
+    # util.console.print(f'test solution 3A: {test_power_rate}')
+    # util.console.print(f'test solution 3B: {test_life_support_rate}\n')
 
     day3_half_bit_length = int(len(day_3_diagnostics) / 2)
     reshaped_array = report_to_bin_array(day_3_diagnostics)
     power_rate = get_power_rate(reshaped_array, day3_half_bit_length)
-    life_support_rate  = get_life_support_rate(day_3_diagnostics)
+
+    shaped = reshape_array(day_3_diagnostics)
+    life_support_rate = get_life_support_rate(shaped)
     util.console.print(f'solution 3A: {power_rate}')
     util.console.print(f'solution 3B: {life_support_rate}\n')
