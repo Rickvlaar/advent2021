eefe0c7b31fe5fe98b8fc025b874675c0e54c36c
RickForce <Rick@Rickforce.com>
2021-12-05 12:04:01 +0100

    tweaked timer functions

--- util.py
+++ util.py
@@ -4,6 +4,7 @@ from rich.console import Console
 
 console = Console(color_system='truecolor')
 
+
 def parse_file_as_list(file):
     return [num.rstrip('\n') for num in open(file=file, newline='\n')]
 
@@ -12,19 +13,35 @@ def convert_str_list_to_int_list(str_list: list[str]) -> list[int]:
     return [int(element) for element in str_list]
 
 
+# Time function runtime in ms
 def get_runtime(function: Callable):
     def wrapper(*args, **kwargs):
         start = time.perf_counter_ns()
         result = function(*args, **kwargs)
         end = time.perf_counter_ns()
-        console.print(f'[bold blue]{function.__name__}[/bold blue]', '[red]ran for[/red]', (end-start)/1e6, '[red]ms[/red]')
+        console.print(f'[bold blue]{function.__name__}[/bold blue]',
+                      '[red]ran for[/red]', (end - start) / 1e6,
+                      '[red]ms[/red]')
         return result
+
     return wrapper
 
 
+# Time function runtime for 'n' iterations in ms
 def time_function(iterations: int, function: Callable, *args, **kwargs):
     start = time.perf_counter_ns()
     for _ in range(iterations):
         function(*args, **kwargs)
     end = time.perf_counter_ns()
-    console.print(f'[bold blue]{function.__name__}[/bold blue]', '[red]ran for[/red]', (end - start) / 1e6,'[red]ms[/red]')
+
+    # Total runtime
+    console.print(f'[bold blue]{function.__name__}[/bold blue]',
+                  f'[red]total runtime for {iterations} iterations[/red]',
+                  (end - start) / 1e6,
+                  '[red]ms[/red]')
+
+    # Averge runtime
+    console.print(f'[bold blue]{function.__name__}[/bold blue]',
+                  f'[red]average runtime for {iterations} iterations[/red]'
+                  , (end - start) / 1e6 / iterations,
+                  '[red]ms[/red]')
