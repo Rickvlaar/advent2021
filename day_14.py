0a3a5ffd83caff6b6c31a7eee315c321b52e5f25
RickForce <Rick@Rickforce.com>
2021-12-17 12:05:07 +0100

    day 14b

--- day_14.py
+++ day_14.py
@@ -3,7 +3,6 @@ from itertools import pairwise
 from collections import Counter, defaultdict
 from functools import reduce
 
-
 day_file = parse_file_as_list('input/day_14.txt')
 test_file = parse_file_as_list('input/day_14_test.txt')
 
@@ -14,61 +13,56 @@ def parse_file(file: list[str]) -> (str, dict[str: str]):
     return polymer, rules
 
 
-def insert_polymers(polymer: str, rules: dict[str, str], pair_dict: dict[str, (str, str)]):
-    new_string = list(polymer)
-
-    offset = 1
-    for index, pair in enumerate(pairwise(polymer)):
-        element_pair = ''.join(pair)
-        if element_pair not in pair_dict:
-            new_char = rules.get(element_pair)
-            pair_dict[element_pair] = (pair[0] + new_char, new_char + pair[1])
+def split_to_pairs(polymer: str) -> list[str]:
+    return [polymer[pos:pos + 2] for pos in range(len(polymer) - 1)]
 
 
-        new_string.insert(index + offset, rules.get(element_pair))
-        offset += 1
-    return new_string
-
-
-def recursive_pair_match(pair: str, rules: dict[str, str],  pair_dict: dict[str, (str, str)], new_chars, max_iter: int, x: int):
-    x += 1
-    if max_iter == x:
+def create_pairs_dict(rules: dict):
+    new_dict = dict()
+    count_dict = dict()
+    for pair, value in rules.items():
         new_char = rules.get(pair)
-        return new_chars.append(new_char)
-
-    if pair in pair_dict:
-        new_pair_1, new_pair_2 = pair_dict.get(pair)
-    else:
-        new_char = rules.get(pair)
-        new_pair_1 = pair[0] + new_char
-        new_pair_2 = new_char + pair[1]
-        pair_dict[pair] = (new_pair_1, new_pair_2)
-
-    recursive_pair_match(new_pair_1, rules, pair_dict, new_chars, max_iter, x)
-    recursive_pair_match(new_pair_2, rules, pair_dict, new_chars, max_iter, x)
+        new_dict[pair] = [pair[0] + new_char, new_char + pair[1]]
+        count_dict[pair] = 0
+    return new_dict, count_dict
 
 
-@time_function()
+def polymerize(rules, pairs_dict: dict[str: list[str]], pairs_count: dict[str: int], char_counter):
+    new_count_dict = {pair: 0 for pair in pairs_count}
+    for pair, count in pairs_count.items():
+        char = rules.get(pair)
+        char_counter[char] += count
+
+        pairs = pairs_dict.get(pair)
+        for subpair in pairs:
+            new_count_dict[subpair] += count
+    return new_count_dict
+
+
+@time_function(100)
 def run(n: int):
     polymer, rules = parse_file(day_file)
+    pairs_dict, count_dict = create_pairs_dict(rules)
 
-    new_polymer = polymer[0]
+    pairs = split_to_pairs(polymer)
+    for pair in pairs:
+        count_dict[pair] += 1
 
-    for y in range(7):
-        new_polymer = polymer[0]
-        for x in range(len(polymer) - 1):
-            new_polymer += recursive_pair_match(polymer[x: x+2], rules, [], y+1, 0) + polymer[x+1]
-        console.print(Counter(new_polymer).most_common())
+    char_counter = defaultdict(int)
+    for char in polymer:
+        char_counter[char] += 1
 
-    most_common = Counter(polymer).most_common()
+    for _ in range(n):
+        count_dict = polymerize(rules, pairs_dict, count_dict, char_counter)
+
+    most_common = Counter(char_counter).most_common()
+
     return most_common[0][1] - most_common[-1][1]
 
 
 if __name__ == '__main__':
     answer_a = run(10)
+    answer_b = run(40)
+
     console.print(f'solution 14A: {answer_a}')
-
-    # answer_b = run(40)
-    # console.print(f'solution 14B: {answer_b}')
-
-
+    console.print(f'solution 14B: {answer_b}')
