fe29e8bd2c9a96b8d91f02339ee5511e22ea45f9
RickForce <Rick@Rickforce.com>
2021-12-17 13:40:22 +0100

    minor performance gains

--- day_17.py
+++ day_17.py
@@ -3,6 +3,7 @@ import itertools
 from util import console, parse_file_as_list, time_function
 from dataclasses import dataclass, field
 from math import prod
+from functools import reduce
 import numpy as np
 
 day_file = parse_file_as_list('input/day_17.txt')
@@ -15,15 +16,6 @@ def parse_target(file: list[str]) -> tuple[tuple[int, ...], tuple[int, ...]]:
 
     x_range_tuple = tuple([int(x) for x in x_raw.removeprefix('x=').split('..')])
     y_range_tuple = tuple([int(y) for y in y_raw.removeprefix('y=').split('..')])
-
-    # answers = []
-    # for bla in file[2:]:
-    #     splitline = bla.split(' ')
-    #     for val in splitline:
-    #         answers.append(val.strip())
-    #
-    # answers.sort()
-    # console.print(answers)
 
     return x_range_tuple, y_range_tuple
 
@@ -46,39 +38,48 @@ def hit_target(position, x_range_tuple, y_range_tuple):
     return x_range_tuple[0] <= position[0] <= x_range_tuple[1] and y_range_tuple[0] <= position[1] <= y_range_tuple[1]
 
 
+def will_never_hit(velocity, x_range_tuple, y_range_tuple):
+    backwards = velocity[0] < 1
+    too_slow = (velocity[0] * (velocity[0] + 1)) // 2 < x_range_tuple[0]
+    too_fast = velocity[0] > x_range_tuple[1] or velocity[1] < y_range_tuple[0]
+
+    return any([backwards, too_slow, too_fast])
+
+
 def missed_target(position, velocity, x_range_tuple, y_range_tuple):
-    undershoot = velocity[0] == 0 and position[0] > x_range_tuple[1]
-    overshoot = velocity[0] == 0 and position[0] < x_range_tuple[0]
     passed_target = position[1] < y_range_tuple[0]
+    overshoot = position[0] > x_range_tuple[1]
+    undershoot = velocity[0] == 0 and position[0] < x_range_tuple[0]
 
-    misses = [undershoot, overshoot, passed_target]
-
-    return any(misses)
+    return any([passed_target, undershoot, overshoot])
 
 
 def project_path(velocity, x_range_tuple, y_range_tuple):
     position = np.array([0, 0])
     max_y = 0
-
     while 1:
         velocity, position = fire(velocity, position)
 
         if position[1] > max_y:
             max_y = position[1]
 
-        if hit_target(position, x_range_tuple, y_range_tuple):
+        if missed_target(position, velocity, x_range_tuple, y_range_tuple):
+            return
+        elif hit_target(position, x_range_tuple, y_range_tuple):
             return max_y
-        elif missed_target(position, velocity, x_range_tuple, y_range_tuple):
-            return None
 
 
-@time_function()
-def run_a(file):
+@time_function(1)
+def run(file):
     x_range_tuple, y_range_tuple = parse_target(file)
-    try_velocities = np.array([start_velocity for start_velocity in itertools.product(np.arange(-250, 250, dtype=int), repeat=2)])
+    try_velocities = np.array(
+            [start_velocity for start_velocity in itertools.product(np.arange(-250, 250, dtype=int), repeat=2)])
 
     hits = []
     for velocity in try_velocities:
+        if will_never_hit(velocity, x_range_tuple, y_range_tuple):
+            continue
+
         hit = project_path(velocity, x_range_tuple, y_range_tuple)
         if hit is not None:
             hits.append(hit)
@@ -86,12 +87,9 @@ def run_a(file):
     hits.sort()
     return hits[-1], len(hits)
 
-def run_b(file):
-    pass
-
 
 if __name__ == '__main__':
-    answer_a, answer_b = run_a(day_file)
+    answer_a, answer_b = run(day_file)
 
     console.print(f'solution 17A: {answer_a}')
     console.print(f'solution 17B: {answer_b}')
